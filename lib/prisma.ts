0b31c17a14bd260b4e608f685ea858b1077f4b23
Maymunah <wr.maymunah@gmail.com>
2026-03-06 13:18:27 +0300

    Bust node memory cache for Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma_new: PrismaClient }
+const globalForPrisma = global as unknown as { prisma_v3: PrismaClient }
 
 export const prisma =
-    globalForPrisma.prisma_new ||
+    globalForPrisma.prisma_v3 ||
     new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma_new = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma_v3 = prisma
